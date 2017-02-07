9fab019c7ece3ce91f69749530afbb42f703dd32
Anna Shipman <anna@annashipman.co.uk>
2017-02-07 20:00:52 +0000

    Add a basic GUI in Java
    I want this to work for anyone. Ideally it would run in a browser but you need to submit the scores and the point of this is that the scores never leave your computer, and browsers cannot update the local filesystem. (There are some libraries that allow you to do it in JavaScript but they are mostly only supported in Chrome and are using deprecated APIs). Hence, Java.
    
    However, I have not used the package struture because the rest of the project won't be in Java. There will be a command line and a graphing function.
    
    This commit is a basic GUI that offers one question and options and prints your choices to the command line. Clearly that's a long way from where I want to be but it's a start.
    
    This commit also ignores the compiled classes.

---
+++ Questions.java
@@ -0,0 +1,102 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+
+public class Questions extends JPanel
+                             implements ActionListener {
+    static String notAtAll = "Not at all";
+    static String severalDays = "Several days";
+    static String moreThanHalf = "More than half the days";
+    static String everyDay = "Nearly every day";
+
+    public Questions() {
+        super(new BorderLayout());
+
+        //Create question
+        JLabel question = new JLabel("Sample question");
+
+        //Create the radio buttons.
+        JRadioButton notAtAllButton = new JRadioButton(notAtAll);
+        notAtAllButton.setMnemonic(KeyEvent.VK_0);
+        notAtAllButton.setActionCommand(notAtAll);
+
+        JRadioButton severalDaysButton = new JRadioButton(severalDays);
+        severalDaysButton.setMnemonic(KeyEvent.VK_1);
+        severalDaysButton.setActionCommand(severalDays);
+
+        JRadioButton moreThanHalfButton = new JRadioButton(moreThanHalf);
+        moreThanHalfButton.setMnemonic(KeyEvent.VK_2);
+        moreThanHalfButton.setActionCommand(moreThanHalf);
+
+        JRadioButton everyDayButton = new JRadioButton(everyDay);
+        everyDayButton.setMnemonic(KeyEvent.VK_3);
+        everyDayButton.setActionCommand(everyDay);
+
+        //Group the radio buttons.
+        ButtonGroup group = new ButtonGroup();
+        group.add(notAtAllButton);
+        group.add(severalDaysButton);
+        group.add(moreThanHalfButton);
+        group.add(everyDayButton);
+
+        //Register a listener for the radio buttons.
+        notAtAllButton.addActionListener(this);
+        severalDaysButton.addActionListener(this);
+        moreThanHalfButton.addActionListener(this);
+        everyDayButton.addActionListener(this);
+
+        //Put the radio buttons in a column in a panel.
+        JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+        radioPanel.add(notAtAllButton);
+        radioPanel.add(severalDaysButton);
+        radioPanel.add(moreThanHalfButton);
+        radioPanel.add(everyDayButton);
+
+        add(question, BorderLayout.WEST);
+        add(radioPanel, BorderLayout.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+    }
+
+    /** Listens to the radio buttons. */
+    public void actionPerformed(ActionEvent e) {
+        //compose this to get what I want the action to be
+        System.out.println(e.getActionCommand());
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("PHQs");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new Questions();
+        newContentPane.setOpaque(true);
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
